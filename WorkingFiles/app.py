5bec5844af6fabd48022d75392fff4f9dd43e0e1
gituseraccount1 <avang067@gmail.com>
2023-10-21 18:12:54 -0500

    updated app.py

--- WorkingFiles/app.py
+++ WorkingFiles/app.py
@@ -19,14 +19,39 @@ def read_data():
     result = []
     for item in data:
         result.append(item)
-    print(jsonify(result))
-    return jsonify(result)
-    
 
-# 4. Define what to do when a user hits the /about route
-@app.route("/test")
-def test():
-    return 'This is working...This is the test route!'
+    # Carbon Monoxide Data only
+    carbonMoData = mongo.db.air_pollution.find({"parameter_name": "Carbon monoxide"}, {'_id': 0, 'parameter_code': 1, 'POC': 1, 'lat': 1, 'lon': 1,'parameter_name': 1,
+                'date_local': 1, 'units_of_measure': 1, 'arithmetic_mean': 1,'first_max_value': 1,
+                'AQI': 1,'state_name': 1, 'county_name': 1, 'city_name': 1}) # will through this "TypeError: Object of type ObjectId is not JSON serializable" if I don't list out all the columns I want. Not sure why.
+
+    carbonMoDataList = []
+    for item in carbonMoData:
+         carbonMoDataList.append(item)
+
+
+    # Get a list/array of the unique chemical/pollutant (for the dropdown list)
+    uniquePollutantsData = mongo.db.air_pollution.aggregate([
+  {
+    "$group": {
+      "_id": "$parameter_name",
+    },
+  },
+ ])
+
+    uniquePollutantsJson = []
+    for item in uniquePollutantsData:
+         uniquePollutantsJson.append(item)
+
+    uniquePollutantsList = [ sub['_id'] for sub in uniquePollutantsJson ] #https://www.geeksforgeeks.org/python-get-values-of-particular-key-in-list-of-dictionaries/
+
+
+
+    # print(jsonify(result))
+    # return jsonify(result)
+    # return jsonify(carbonMoDataList)
+    # return jsonify(uniquePollutantsList)
+    return render_template('index.html', data1=result, data2=carbonMoDataList, data3=uniquePollutantsList)
 
 
 if __name__ == "__main__":
